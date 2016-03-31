7bd591e5ee4559cdd2e1a2e65f11348e430520b7
Jonathan Stroud <stroud@umich.edu>
2016-03-30 22:21:19 -0400

    pbs preempt

--- flux/interactive_flux.sh
+++ flux/interactive_flux.sh
@@ -6,7 +6,7 @@ qsub \
 -m abe \
 -A mdatascienceteam_flux \
 -q flux \
--l qos=flux,nodes=1:ppn=1,pmem=4gb,walltime=00:04:00:00 \
+-l qos=preempt,nodes=1:ppn=1,pmem=4gb,walltime=00:04:00:00 \
 -j oe \
 -V \
 -d "/scratch/mdatascienceteam_flux/uniqname/"
